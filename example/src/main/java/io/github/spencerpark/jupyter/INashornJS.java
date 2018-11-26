303d4ece608e027566eb3bf1f8fecf9c85d464c4
SpencerPark <spinnr95@gmail.com>
2018-11-25 22:33:46 -0500

    Add example of creating a configurable installer.

--- example/src/main/java/io/github/spencerpark/jupyter/INashornJS.java
+++ example/src/main/java/io/github/spencerpark/jupyter/INashornJS.java
@@ -7,6 +7,7 @@ import io.github.spencerpark.jupyter.kernel.KernelConnectionProperties;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.logging.Level;
 
 public class INashornJS {
@@ -26,7 +27,13 @@ public class INashornJS {
         KernelConnectionProperties connProps = KernelConnectionProperties.parse(contents);
         JupyterConnection connection = new JupyterConnection(connProps);
 
-        NashornKernel kernel = new NashornKernel();
+        String envEngineArgs = System.getenv("JS_ENGINE_ARGS");
+        if (envEngineArgs == null)
+            envEngineArgs = "-scripting";
+
+        String[] engineArgs = envEngineArgs.split(" ");
+
+        NashornKernel kernel = new NashornKernel(engineArgs);
         kernel.becomeHandlerForConnection(connection);
 
         connection.connect();
