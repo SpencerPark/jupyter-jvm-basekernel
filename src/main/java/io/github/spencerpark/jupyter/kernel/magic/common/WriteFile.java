314c43cc9c1f72c2a4cb286f4e150e59fbba850a
SpencerPark <spinnr95@gmail.com>
2018-12-29 17:13:57 -0500

    Add common display magics and update writefile to be more consistent with canonical.

--- src/main/java/io/github/spencerpark/jupyter/kernel/magic/common/WriteFile.java
+++ src/main/java/io/github/spencerpark/jupyter/kernel/magic/common/WriteFile.java
@@ -1,30 +1,36 @@
 package io.github.spencerpark.jupyter.kernel.magic.common;
 
 import io.github.spencerpark.jupyter.kernel.magic.registry.CellMagic;
+import io.github.spencerpark.jupyter.kernel.magic.registry.MagicsArgs;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.List;
+import java.util.Map;
 
 public class WriteFile {
+    private static final MagicsArgs WRITEFILE_ARGS = MagicsArgs.builder()
+            .required("filename")
+            .flag("append", 'a')
+            .onlyKnownFlags().onlyKnownKeywords()
+            .build();
 
     @CellMagic
     public static Void writefile(List<String> args, String body) throws Exception {
-        if (args.isEmpty())
-            throw new IllegalArgumentException("WriteFile magic expects a file name as an argument but one was not given");
+        Map<String, List<String>> vals = WRITEFILE_ARGS.parse(args);
 
-        String fileName = args.get(0);
+        String fileName = vals.get("filename").get(0);
+        boolean append = !vals.get("append").isEmpty();
 
         File file = new File(fileName);
 
         if (file.isDirectory())
             throw new FileAlreadyExistsException("Cannot write to file " + fileName + ". It is a directory.");
 
-        file.createNewFile();
-
-        try (OutputStreamWriter fileOut = new OutputStreamWriter(new FileOutputStream(file))) {
+        try (OutputStreamWriter fileOut = new OutputStreamWriter(new FileOutputStream(file, append), Charset.forName("utf8"))) {
             fileOut.write(body);
         }
 
